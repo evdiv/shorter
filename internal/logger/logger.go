2e7ad397ce93a1afc8d4d6d6784451dee3ebd949
Eugene Dv <vakuka@gmail.com>
2025-02-13 21:55:04 -0500

    Fix compile error

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,5 +1,26 @@
-package main
+package logger
 
-func main() {
+import (
+	"go.uber.org/zap"
+	"log"
+)
 
+var sugar *zap.SugaredLogger
+
+// InitLogger - inits the logger
+func InitLogger() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(" Zap Logger init error", err)
+	}
+	defer logger.Sync()
+	sugar = logger.Sugar()
+}
+
+// GetLogger - returns the existing logger or creates the new one
+func GetLogger() *zap.SugaredLogger {
+	if sugar == nil {
+		InitLogger()
+	}
+	return sugar
 }
--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -1,5 +1,42 @@
-package main
+package middleware
 
-func main() {
+import (
+	"net/http"
+	"shorter/internal/logger"
+	"time"
+)
 
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	status int
+	size   int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.status = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (lrw *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := lrw.ResponseWriter.Write(b)
+	lrw.size += size
+	return size, err
+}
+
+func WithLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		start := time.Now()
+
+		lrw := &loggingResponseWriter{ResponseWriter: w}
+		next.ServeHTTP(lrw, r)
+
+		logger.GetLogger().Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", lrw.status,
+			"size", lrw.size,
+			"duration", time.Since(start),
+		)
+	})
 }
